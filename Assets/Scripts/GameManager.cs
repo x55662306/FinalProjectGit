6c115e65336a838f5efa9af25cfefb6fdb815a7e
x55662306 <zaqxsw1475369@gmail.com>
2020-05-31 15:30:25 +0800

    1.部分建築物 2. 目標光點(送單用)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public int score = 100;
-    public float time = 100;
-    public Text scoreText;
-    public Text timeText;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +15,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        countDown();
-        displayUI();
+        
     }
 
-    void displayUI()
-    {
-        scoreText.text = score.ToString();
-        timeText.text = Mathf.RoundToInt(time).ToString();
-    }
-
-    void countDown()
-    {
-        time -= Time.deltaTime;
-    }
 }
---
+++ Assets/Scripts/UIManagerGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerGame: MonoBehaviour
+{
+    static public int score = 100;
+    public float time = 100;
+    public Text scoreText;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown();
+        displayUI();
+    }
+
+    void displayUI()
+    {
+        scoreText.text = score.ToString();
+        timeText.text = Mathf.RoundToInt(time).ToString();
+    }
+
+    void countDown()
+    {
+        time -= Time.deltaTime;
+    }
+}
