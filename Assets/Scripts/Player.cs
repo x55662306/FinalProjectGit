197c49f7123b4abcb80f2afac1083ea78ed5d867
eric40325 <eric40325@g.ncu.edu.tw>
2020-06-03 06:41:18 +0800

    加上跑步動畫

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float velocity;
-
-
+    public Animator anim;
+    public int state = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        state = 0;
+        if (Input.GetKey(KeyCode.W)) {
             this.transform.position += Vector3.forward * velocity * Time.deltaTime;
-        if (Input.GetKey(KeyCode.A))
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            state = 1;
+        }
+        if (Input.GetKey(KeyCode.A)) { 
             this.transform.position += Vector3.left * velocity * Time.deltaTime;
-        if (Input.GetKey(KeyCode.S))
+            this.transform.rotation = Quaternion.Euler(0, 270, 0);
+            state = 1;
+        }
+        if (Input.GetKey(KeyCode.S)) { 
             this.transform.position += Vector3.back * velocity * Time.deltaTime;
-        if (Input.GetKey(KeyCode.D))
+            this.transform.rotation = Quaternion.Euler(0, 180, 0);
+            state = 1;
+        }
+        if (Input.GetKey(KeyCode.D)) { 
             this.transform.position += Vector3.right * velocity * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(0, 90, 0);
+            state = 1;
+        }
+        anim.SetInteger("State", state);
     }
 }
