524d8e4985bb2c2ec9d5e92ad38d3d4fc02b6ca1
eric40325 <eric40325@g.ncu.edu.tw>
2020-06-14 02:05:07 +0800

    加角色動畫

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Animator anim;
     public float velocity;
     public float rotateVelocity;
     public int health;
-
+    public int state = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         health = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.E))
+        state = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
             this.transform.position += this.transform.forward * velocity * Time.deltaTime;
+            state = 1;
+        }
         if (Input.GetKey(KeyCode.A))
+        {
             this.transform.Rotate(0, -rotateVelocity * Time.deltaTime, 0);
+        }
         if (Input.GetKey(KeyCode.S))
+        {
             this.transform.position += -this.transform.forward * velocity * Time.deltaTime;
+            state = 1;
+        }
         if (Input.GetKey(KeyCode.D))
+        {
             this.transform.Rotate(0, rotateVelocity * Time.deltaTime, 0);
+        }
+        anim.SetInteger("State", state);
     }
 
     public void AddHealth(int deltaHealth)
