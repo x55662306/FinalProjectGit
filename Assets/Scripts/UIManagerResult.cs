466be71a5b4ab7a02703191a97b201ed8e35fc8c
x55662306 <zaqxsw1475369@gmail.com>
2020-06-14 14:43:25 +0800

    結算畫面美化

--- Assets/Scripts/UIManagerResult.cs
+++ Assets/Scripts/UIManagerResult.cs
@@ -6,16 +6,26 @@ using UnityEngine.UI;
 public class UIManagerResult : MonoBehaviour
 {
     public Text textResult;
+    public RawImage rawImage;
 
     // Start is called before the first frame update
     void Start()
     {
         textResult.text = StaticVarible.score.ToString();
+        SetImage();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetImage()
+    {
+        if (StaticVarible.victory)
+            rawImage.texture = Resources.Load<Texture>("Sprites/Victory");
+        else
+            rawImage.texture = Resources.Load<Texture>("Sprites/Fail");
     }
 }
