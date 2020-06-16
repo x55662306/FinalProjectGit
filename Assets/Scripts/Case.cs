88923cdccef33b7e2c12961f19d1e45368b407b7
eric40325 <eric40325@g.ncu.edu.tw>
2020-06-16 12:10:49 +0800

    加入村民和接觸動畫

--- Assets/Scripts/Case.cs
+++ Assets/Scripts/Case.cs
@@ -4,62 +4,74 @@ using UnityEngine;
 
 public class Case : MonoBehaviour
 {
+    
     public enum State
     {
         support,
         recieve
-    }
+    }    
 
-    public GameObject targetTipPrefab;
-
+    public Animator anim;
     private State state;
     private GameObject targetTip;
     private GameObject gameManager;
     private GameObject player;
     private GameObject spawner;
-    private int id;
+    public GameObject targetTipPrefab;
+    private Player playerScript;
     private string caseType;
+    private int id;
     private int reward;
     private int toxicity;
-    
+    public int action = 0;
 
-
-    // Sart is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         gameManager = GameObject.Find("GameManager");
         player = GameObject.Find("Player");
+        playerScript = GameObject.Find("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        anim.SetInteger("action", action);
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Over"))
+            Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "Player" && state == State.support)
+        if (other.name == "Player" && state == State.support && spawner.GetComponent<Spawner>().getFull())
         {
             gameManager.GetComponent<GameManager>().spawnSpecificCase(State.recieve, id);
             spawner.GetComponent<Spawner>().setFull(false);
             Destroy(targetTip);
-            Destroy(gameObject);
+            action = 2;
+            playerScript.SetAction(2);
         }
-        else if (other.name == "Player" && state == State.recieve)
+        else if (other.name == "Player" && state == State.recieve && spawner.GetComponent<Spawner>().getFull())
         {
             StaticVarible.score += reward;
             player.GetComponent<Player>().AddHealth(toxicity*StaticVarible.diffculty);
             Destroy(targetTip);
             gameManager.GetComponent<GameManager>().spawnRandomCase(State.support);
             spawner.GetComponent<Spawner>().setFull(false);
-            Destroy(gameObject);
+            action = 1;
+            playerScript.SetAction(3);
         }
     }
 
     public void SetState(State state)
     {
         this.state = state;
+    }
+
+    public State getState()
+    {
+        return state;
     }
 
     public void SetInfo(State state, int id, GameObject spawner)
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public enum State
+    {
+        support,
+        recieve
+    }
+    
     public Animator anim;
+    private State state;
+    private GameObject spawner;
     public float velocity;
     public float rotateVelocity;
     public float moveSpeed;
     public int health;
-    public int state = 0;
+    public int action = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,30 +30,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        state = 0;
+        if(action<2)
+            action = 0;
         float mouseX = Input.GetAxis("Mouse X") * moveSpeed;
         float mouseY = Input.GetAxis("Mouse Y") * moveSpeed;
         //Camera.main.transform.localRotation = Camera.main.transform.localRotation * Quaternion.Euler(-mouseY, 0, 0);
         transform.localRotation = transform.localRotation * Quaternion.Euler(0, mouseX, 0);
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && action < 2)
         {
             this.transform.position += this.transform.forward * velocity * Time.deltaTime;
-            state = 1;
+            action = 1;
         }
         if (Input.GetKey(KeyCode.A))
         {
             this.transform.Rotate(0, -rotateVelocity * Time.deltaTime, 0);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && action < 2)
         {
             this.transform.position += -this.transform.forward * velocity * Time.deltaTime;
-            state = 1;
+            action = 1;
         }
         if (Input.GetKey(KeyCode.D))
         {
             this.transform.Rotate(0, rotateVelocity * Time.deltaTime, 0);
         }
-        anim.SetInteger("State", state);
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Stand") && action > 1)
+            action=0;
+        anim.SetInteger("action", action);
     }
 
     public void AddHealth(int deltaHealth)
@@ -60,5 +72,10 @@ public class Player : MonoBehaviour
     public void SetHealth(int health)
     {
         this.health = health;
+    }
+
+    public void SetAction(int action)
+    {
+        this.action = action;
     }
 }
